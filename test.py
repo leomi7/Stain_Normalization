36dfdb366c4328af6bcb615a2e811f80f9802b03
student <leomi7ayyar@hpc.iiitd.edu.in>
2018-03-14 16:51:32 +0530

    p

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import stain_utils as utils
-import stainNorm_Reinhard
+import stainNorm_Macenko
 
 import cv2
 
 i3 = utils.read_image('./data/i3.png')
 i2 = utils.read_image('./data/i1.png')
-n = stainNorm_Reinhard.Normalizer()
+n = stainNorm_Macenko.Normalizer()
 n.fit(i3)
 out = n.transform(i2)
 cv2.imwrite('ans.jpg',out)
