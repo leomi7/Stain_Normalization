c800980f39ba4607b7d70d03a830ea951f3dffc1
student <leomi7ayyar@hpc.iiitd.edu.in>
2018-03-14 16:47:33 +0530

    p

--- test.py
+++ test.py
@@ -4,8 +4,7 @@ import stainNorm_Reinhard
 import cv2
 
 i3 = utils.read_image('./data/i3.png')
-i2 = utils.read_image('./data/i2.png'
-)
+i2 = utils.read_image('./data/i1.png')
 n = stainNorm_Reinhard.Normalizer()
 n.fit(i3)
 out = n.transform(i2)
